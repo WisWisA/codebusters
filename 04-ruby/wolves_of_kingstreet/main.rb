c77ab3f6eebbf06e5015a00af67cb90aff28416e
Daniel Tsui <tsui.daniel@gmail.com>
2018-04-06 14:04:57 +1000

    add check for nil

--- 04-ruby/wolves_of_kingstreet/main.rb
+++ 04-ruby/wolves_of_kingstreet/main.rb
@@ -7,11 +7,14 @@ get '/' do
   erb :index
 end
 
-# http://localhost:4567/stock_info?stock_symbol=
+# http://localhost:4567/stock_info?stock_symbol=nflx
 get '/stock_info' do
   redirect to('/') if params[:stock_symbol].empty?  
      
   stock_info = StockQuote::Stock.quote(params[:stock_symbol]) 
+  
+  redirect to('/') if stock_info.nil?  
+
   @price = stock_info.latest_price
   erb :stock_info
 end
